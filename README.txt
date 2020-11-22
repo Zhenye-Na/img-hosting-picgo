fdc4c529b20485f9fc4c2c70f40c41630b700f38
Na, Zhenye <nazhenye@gmail.com>
2020-11-22 01:12:01 -0800

    remove changes

---
+++ README.txt
@@ -0,0 +1,1 @@
+Image Hosting Repo for PicGo
--- sidebars.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  someSidebar: {
-    "CMU 15-445": [
-      "cmu-15445/lecture1",
-    ],
-    Others: ["doc1"],
-    Features: ['mdx'],
-  },
-};
